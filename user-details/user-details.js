99b2d36231f05a56b84e8890eb0582985e561441
akhodyriev2 <akhodyriev2@gmail.com>
2024-01-12 19:03:24 +0200

    details updated

--- user-details/user-details.js
+++ user-details/user-details.js
@@ -9,46 +9,52 @@ const userId = new URL(location.href).searchParams.get('userId');
 const foo = async () => {
     const response = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`);
     const user = await response.json();
-    console.log(userId);
 
     const wrap = document.getElementById('wrap');
     const ul = document.createElement('ul');
     const id = document.createElement('li');
+    const name = document.createElement('li');
+    const phone = document.createElement('li');
     const username = document.createElement('li');
-    const email = document.createElement('li');
+    const website = document.createElement('li');
+
+    const city = document.createElement('li');
+    const geolat = document.createElement('li');
+    const geolng = document.createElement('li');
     const street = document.createElement('li');
     const suite = document.createElement('li');
-    const city = document.createElement('li');
     const zipcode = document.createElement('li');
-    const lat = document.createElement('li');
-    const lng = document.createElement('li');
-    const phone = document.createElement('li');
-    const website = document.createElement('li');
+
+    const companybs = document.createElement('li');
+    const companycatchPhrase = document.createElement('li');
     const companyName = document.createElement('li');
-    const catchPhrase = document.createElement('li');
-    const bs = document.createElement('li');
-    const post = document.createElement('li');
+
+    const email = document.createElement('li');
+
     const button = document.createElement('button');
 
-
     id.textContent = `id: ${user.id}`;
+    name.textContent = `name: ${user.name}`;
+    phone.textContent = `phone: ${user.phone}`;
     username.textContent = `username: ${user.username}`;
-    email.textContent = `email: ${user.email}`;
+    website.textContent = `website: ${user.website}`;
+
+    city.textContent = `city: ${user.address.city}`;
+    geolat.textContent = `geolat: ${user.address.geo.lat}`;
+    geolng.textContent = `geolng: ${user.address.geo.lng}`;
     street.textContent = `street: ${user.address.street}`;
     suite.textContent = `suite: ${user.address.suite}`;
-    city.textContent = `city: ${user.address.city}`;
     zipcode.textContent = `zipcode: ${user.address.zipcode}`;
-    lat.textContent = `lat: ${user.address.geo.lat}`;
-    lng.textContent = `lng: ${user.address.geo.lng}`;
-    phone.textContent = `phone: ${user.phone}`;
-    website.textContent = `website: ${user.website}`;
+
+    companybs.textContent = `companybs: ${user.company.bs}`;
+    companycatchPhrase.textContent = `companycatchPhrase: ${user.company.catchPhrase}`;
     companyName.textContent = `companyName: ${user.company.name}`;
-    catchPhrase.textContent = `catchPhrase: ${user.company.catchPhrase}`;
-    bs.textContent = `bs: ${user.company.bs}`;
-    post.textContent = 'post: ${user.post}';
+
+    email.textContent = `email: ${user.email}`;
+
     button.innerText = 'post of current user';
 
-    ul.append(id, username, email, street, suite, city, zipcode, lat, lng, phone, website, companyName, catchPhrase, bs, button);
+    ul.append(id, name, phone, username, website, city, geolat, geolng, street, suite, zipcode, companybs, companycatchPhrase, companyName, email, button);
     wrap.appendChild(ul);
     console.log(user);
 
